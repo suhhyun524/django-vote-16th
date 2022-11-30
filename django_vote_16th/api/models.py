4b1b506457e6320db31de28c8295dda2fade62cf
chaeshee0908 <chaeshee0908@naver.com>
2022-11-30 21:55:48 +0900

    🚀feat: model 생성

--- django_vote_16th/api/models.py
+++ django_vote_16th/api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel):
+    user_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    part = models.CharField(max_length=30)
+    password = models.CharField(max_length=150)
+    team = models.CharField(max_length=30)
+    is_voted_demo = models.BooleanField(default=False)
+    is_voted_partleader = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Demo_Vote(BaseModel):
+    team = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.team
+
+
+class PartLeader_Vote(BaseModel):
+    votee = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.votee
