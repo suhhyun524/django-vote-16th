d2acffb84ec91e50b74ed3787a3c30053322a80c
suhhyun <2835223@ewhain.net>
2022-12-04 22:35:51 +0900

    Add vote views&urls

--- django_vote_16th/vote/urls.py
+++ django_vote_16th/vote/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', VoteView.as_view(), name='vote'),
-    path('result/', ResultView.as_view(), name='result')
+    path('demo/', DemoVoteView.as_view(), name='demo-vote'),
+    path('part/<str:part>/', PartVoteView.as_view(), name='part-vote'),
+    path('result/demo/', DemoResultView.as_view(), name='result'),
+    path('result/part/<str:part>/', PartResultView.as_view(), name='result')
 ]
--- django_vote_16th/vote/views.py
+++ django_vote_16th/vote/views.py
@@ -1,29 +1,65 @@
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.db.models import Count
 from drf_yasg.utils import swagger_auto_schema
+from api.models import User
+from api.serializers import TeamSerializer, UserSerializer
 from .models import Demo_Vote, PartLeader_Vote
 
 from .open_api_params import *
 
 
 # Create your views here.
-class VoteView(APIView):
+class DemoVoteView(APIView):
     permissions_classes = [permissions.AllowAny]
 
-    @swagger_auto_schema(manual_parameters=get_params)
-    def get(self, request):
-        return Response("Swagger 연동 테스트")
+    @swagger_auto_schema()
+    def get(self, request, part):
+        lists = User.objects.all()
+        serializer = TeamSerializer(lists, many=True)
 
-    @swagger_auto_schema(request_body=post_params)
+        return Response(serializer.data)
+
+    @swagger_auto_schema(request_body=demo_vote)
     def post(self, request):
-        return Response("Swagger post 테스트")
+
+        # if(User.team!=request.body['team'])
+        Demo_Vote.object.create(team=request.body['team'])
+
+        return Response("데모 데이 투표 성공")
 
 
-class ResultView(APIView):
+class PartVoteView(APIView):
     permissions_classes = [permissions.AllowAny]
 
-    @swagger_auto_schema(manual_parameters=get_params)
+    @swagger_auto_schema(manual_parameters=part_get)
+    def get(self, request, part):
+        lists = User.objects.filter(part=part)
+        serializer = UserSerializer(lists, many=True)
+
+        return Response(serializer.data)
+
+    @swagger_auto_schema(request_body=part_vote)
+    def post(self, request):
+        PartLeader_Vote.object.create(votee=request.body['votee'])
+
+        return Response("파트 리더 투표 성공")
+
+
+class DemoResultView(APIView):
+    permissions_classes = [permissions.AllowAny]
+
+    @swagger_auto_schema()
     def get(self, request):
-        Demo_Vote.objects.all()
-        return Response("This is result page")
+        lists = Demo_Vote.objects.all().values('team').annotate(total=Count('team')).order_by('-total')
+        return Response(lists)
+
+
+class PartResultView(APIView):
+    permissions_classes = [permissions.AllowAny]
+
+    @swagger_auto_schema()
+    def get(self, request, part):
+        lists = PartLeader_Vote.objects.filter(part=part).values('votee').annotate(total=Count('votee')).order_by('-total')
+        return Response(lists)
