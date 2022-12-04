4c5723e8e1327af6cf088de8584cb4cd92e38e8e
suhhyun <2835223@ewhain.net>
2022-12-04 20:17:21 +0900

    Update Model

--- django_vote_16th/vote/models.py
+++ django_vote_16th/vote/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Demo_Vote(BaseModel):
+    team = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.team
+
+
+class PartLeader_Vote(BaseModel):
+    votee = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.votee
