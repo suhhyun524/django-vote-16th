56e9239da56c3e3c77ee3a180a609e1fb0353a2e
chaeshee0908 <chaeshee0908@naver.com>
2022-12-08 21:47:10 +0900

    access token 재발급

--- django_vote_16th/user/urls.py
+++ django_vote_16th/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import *
 
 urlpatterns = [
     path('join/', JoinAPIView.as_view(), name='join'),
-    path('login/', LoginAPIView.as_view(), name='login')
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token refresh')
 ]
