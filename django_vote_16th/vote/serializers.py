21c7794b0dab771f202938e83ed88817b41b7381
suhhyun <2835223@ewhain.net>
2022-12-11 21:20:33 +0900

    Add VoteSerializers

--- django_vote_16th/vote/serializers.py
+++ django_vote_16th/vote/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import *
+from user.models import User
+from .models import Demo_Vote, PartLeader_Vote
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,3 +13,24 @@ class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['team']
+
+
+class DemoVoteSerializer(serializers.ModelSerializer):
+    total = serializers.IntegerField(
+        read_only=True
+    )
+
+    class Meta:
+        model = Demo_Vote
+        fields = ['team', 'total']
+
+
+class PartVoteSerializer(serializers.ModelSerializer):
+    total = serializers.IntegerField(
+        read_only=True
+    )
+
+    class Meta:
+        model = PartLeader_Vote
+        fields = ['votee', 'total']
+
