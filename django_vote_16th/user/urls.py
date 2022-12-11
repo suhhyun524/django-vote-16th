7736afb411c10b33ebbd2492c271840371494b4d
chaeshee0908 <chaeshee0908@naver.com>
2022-12-11 15:51:53 +0900

    토큰 유효성 검사 커스터마이징

--- django_vote_16th/user/urls.py
+++ django_vote_16th/user/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from .views import *
 
 urlpatterns = [
     path('join/', JoinAPIView.as_view(), name='join'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshAPIView.as_view(), name='token_refresh'),
-    path('login/verify/', TokenVerifyView.as_view(), name='token_verify')
+    path('login/verify/', TokenVerifyAPIView.as_view(), name='token_verify')
 ]
