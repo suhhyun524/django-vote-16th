1bac35906432ba7d4f96dae8dd1706da4cd1e0bd
suhhyun <2835223@ewhain.net>
2022-12-16 14:26:18 +0900

    Update vote/result/part

--- django_vote_16th/vote/urls.py
+++ django_vote_16th/vote/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('demo/', DemoVoteView.as_view(), name='demo-vote'),
     path('part/', PartVoteView.as_view(), name='part-vote'),
     path('result/demo/', DemoResultView.as_view(), name='result'),
-    path('result/part/', PartResultView.as_view(), name='result')
+    path('result/part/<str:part>', PartResultView.as_view(), name='result')
 ]
